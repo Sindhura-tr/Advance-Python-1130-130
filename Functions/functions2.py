cd0d807738db579a39a02fe9bef728b4a47e3478
Sindhura-tr <sindhurandst.etlhive@gmail.com>
2025-04-23 12:11:53 +0530

    functions-introdcured args

---
+++ Functions/functions2.py
@@ -0,0 +1,18 @@
+# def add2(x,y):
+#     """
+#     returns addition of given input data
+#     """
+#     c1 = x+y
+#     return c1
+
+# a1 = add2(15,90)
+# print(a1)
+
+# add2(89,67)
+
+def multiply(x,y):
+    return x*y
+
+m1 = multiply(89,78)
+print(m1)
+print(multiply(67,45))
